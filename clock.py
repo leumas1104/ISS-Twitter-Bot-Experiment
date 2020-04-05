21073ab4cde925d999d1ee3b251b5d98153a2974
leumas1104 <63166690+leumas1104@users.noreply.github.com>
2020-04-05 21:34:57 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,5 +1,11 @@
 import TwitterBot
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+@sched.run_prog('interval', minutes=10)
+def run_prog():
+    TwitterBot.main()
 
+sched.start()
+    
+    
     
