18cdde87d11a3fe6536fcc181ebea7dad69557d8
leumas1104 <63166690+leumas1104@users.noreply.github.com>
2020-04-05 15:03:44 +0200

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -7,6 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def test():
+    return "Hi"
     fileName = "lastID.txt"
     print('test')
     #from keys.py in the same file directory
