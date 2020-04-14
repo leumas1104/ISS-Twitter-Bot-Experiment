5c006ebeb9f86362fc85dc4cfe3365c7acc76d74
leumas1104 <63166690+leumas1104@users.noreply.github.com>
2020-04-14 15:20:50 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,6 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=10)
 def timed_job():
+    print("10 Minutes are over!")
     TwitterBot.TwitterBot()
 
 sched.start()
